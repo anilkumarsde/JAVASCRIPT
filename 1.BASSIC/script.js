6c95f0a8e3f64ee3ee0a51e5706e487eab44e9fd
anilkumarsde <anilkumarlks16@gmail.com>
2024-07-02 22:40:05 +0530

    updated

---
+++ 1.BASSIC/script.js
@@ -0,0 +1,22 @@
+// const student= {
+//     fullname:"ANIL kumar",
+//     age:24,
+//     cgpa:8.5,
+// };
+// console.log(student["age"]);
+// console.log(student["fullname"]);
+
+const product ={
+    productName:"pen",
+    rating:5,
+    price:279,
+    offer:56
+
+};
+console.log(product);
+console.log(product.offer);
+console.log(product.rating);
+console.log(product.price);
+console.log(product.productName);
+
+
