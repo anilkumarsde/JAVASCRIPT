33b1197ebc43b27cc109802ed1781d43b5d98152
anilkumarsde <anilkumarlks16@gmail.com>
2024-07-04 23:13:52 +0530

    updated

---
+++ 3.LOOP/script.js
@@ -0,0 +1,23 @@
+for(let i=0;i<=5;i++)
+{
+    console.log("HELLO WORLD!",i);
+}
+//LOOP IS USED TO EXECUTE A PIECE OF CODE AGAIN AND AGAIN
+//WRITE A PROGRAM TO CALCULATE SUM OF FIRST N NUMBER
+
+// let sum = 0;
+// for (let i = 0;i<5;i++)
+// {
+//     sum+=i;
+// }
+// console.log("sum =",sum);//sum = 15
+// console.log("loop has ended");
+
+//while loop
+
+let i=0;
+while(i<9)
+{
+    console.log("i=",i);
+
+}
