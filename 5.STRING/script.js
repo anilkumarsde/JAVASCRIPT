1b2be74b92656118c6785503323d88192b4e9016
anilkumarsde <anilkumarlks16@gmail.com>
2024-07-05 21:39:22 +0530

    updated

---
+++ 5.STRING/script.js
@@ -0,0 +1,12 @@
+// let str="anil kumar";
+
+// to calculate length of string
+
+// console.log(str.length)
+
+// console.log(str[8]);
+
+//template literals
+
+let specialstring=`this is a template literals`;
+console.log(specialstring);
