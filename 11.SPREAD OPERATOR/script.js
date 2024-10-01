3552efcfcf6bc0d5a52f993ba308ac09ab7fa90c
anilkumarsde <anilkumarlks16@gmail.com>
2024-10-01 20:08:25 +0530

    updated

---
+++ 11.SPREAD OPERATOR/script.js
@@ -0,0 +1,16 @@
+// let newStudent = [{ name: "Pawan", age: 25 }];
+// let student = [{ name: "anil", age: 23 }, ...newStudent];
+// let NewS=[...student,...newStudent];
+// NewS.forEach((val)=>{
+//     console.log(val.name)
+//     console.log(val.age)
+// })
+// console.log(student);
+// let arr=[5,6,7,];
+// let arr1=[2,3,4];
+// let newArr=[...arr,arr1];
+// console.log(newArr)
+
+let arr=[5,6,7,8]
+let newArr=[10,...arr];
+console.log(newArr)
