4dec9e8f7a83bd30b2e994c7fa8442d50d06d20f
anilkumarsde <anilkumarlks16@gmail.com>
2024-10-01 19:40:36 +0530

    updated

---
+++ 10.ES6/script.js
@@ -0,0 +1,53 @@
+// let fruit=['Apple','Banana','Mango','Kiwi'];
+// let fruit1=fruit[0];  old tarika
+// console.log(fruit1)
+// let fruit2=fruit[1];
+// console.log(fruit2);
+// let fruit3=fruit[3];
+// console.log(fruit3)
+// let [fruit1,fruit2,fruit3,fruit4]=fruit  // New tarika
+// console.log(fruit1)
+// console.log(fruit2)
+// console.log(fruit3)
+// let [fruit1,,fruit3,fruit4,fruit5='Gawava']=fruit;
+// console.log(fruit1)
+// console.log(fruit3)
+// console.log(fruit4)
+// console.log(fruit5)
+// let [fruit1,...fruitX]=fruit;  // rest operator
+// console.log(fruitX)
+
+// let user={
+//     sname:'Anil',
+//     age:23,
+//     mobile:9999999,
+//     email:'anil@gmail.com'
+// }
+// let {sname,email,age}=user;
+// console.log(sname)
+// console.log(age)
+
+// let fruit=['Apple','banana','kiwi','mango'];
+// let otherFruit=['lichi','amrudh'];
+// let allFruit=[...fruit,...otherFruit]
+// allFruit.map((val)=>{
+//     console.log(val+"Anil")
+// })
+// function printAll(a,b,c,d,e){
+//     console.log(a)
+
+
+// }
+// printAll(fruit[0],fruit[1],fruit[2]) // old tarika
+
+// printAll(...fruit) // spread operator
+
+// let fruit=['apple','mango','lichi']
+// let otherFruit=['amrudh','angur',...fruit]
+// console.log(otherFruit)
+// let fruit=['apple','lichi'];
+// let otherFruit=[...fruit];
+// otherFruit.push('test');
+// console.log(otherFruit)
+// console.log(fruit)
+
