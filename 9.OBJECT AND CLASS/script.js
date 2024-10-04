904c099b0f8f8c84368243fee75c6cc01021ed55
anilkumarsde <anilkumarlks16@gmail.com>
2024-10-04 12:11:09 +0530

    updated

--- 9.OBJECT AND CLASS/script.js
+++ 9.OBJECT AND CLASS/script.js
@@ -1,9 +1,33 @@
-student={
-    fullName:'anil',
-    age:23,
-    education:'b.tech',
-    marks:95,
-   printmaks:()=>(
-    console.log(`marks is ${marks}`)
-   )
-}
+student = {
+  fullName: "Anil kumar",
+  age: 23,
+  marks: 55.6,
+  getMarks: function () {
+    console.log("marks", marks);
+  },
+};
+const employee = {
+  caltax() {
+    console.log("tax rate is 10%");
+  },
+};
+const karanArjun={
+    salary:50000,
+};
+const karanArjun2={
+    salary:50000,
+};
+const karanArjun3={
+    salary:50000,
+    caltax() {
+        console.log("tax rate is 30%");
+      },
+};
+const karanArjun4={
+    salary:50000,
+};
+karanArjun.__proto__=employee;
+karanArjun2.__proto__=employee;
+karanArjun3.__proto__=employee;
+karanArjun4.__proto__=employee;
+
